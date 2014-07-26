963b8339c6e76a9bcc0edb1be85fd1c3239a8576
pauldmps <shantanupaul@live.com>
2014-07-26 21:18:04 +0530

    updated swipe down to refresh animation

--- gen/com/paulshantanu/bputapp/R.java
+++ gen/com/paulshantanu/bputapp/R.java
@@ -1220,9 +1220,9 @@ containing a value of this type.
         public static final int status_bar_opened_default_background=0x7f02005b;
     }
     public static final class id {
-        public static final int HorizontalScrollView1=0x7f050042;
+        public static final int HorizontalScrollView1=0x7f050043;
         public static final int LinearLayout1=0x7f05003c;
-        public static final int RelativeLayout1=0x7f05003f;
+        public static final int RelativeLayout1=0x7f050040;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1234,7 +1234,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050044;
+        public static final int action_settings=0x7f050045;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1255,17 +1255,17 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int lv_notices_main=0x7f05003e;
+        public static final int lv_notices_main=0x7f05003f;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
-        public static final int notice_table=0x7f050043;
-        public static final int notice_text=0x7f050041;
+        public static final int notice_table=0x7f050044;
+        public static final int notice_text=0x7f050042;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
-        public static final int scrollView1=0x7f050040;
+        public static final int scrollView1=0x7f050041;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1282,7 +1282,8 @@ containing a value of this type.
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
-        public static final int swipe_container=0x7f05003d;
+        public static final int swipe_container=0x7f05003e;
+        public static final int swipe_hint=0x7f05003d;
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
